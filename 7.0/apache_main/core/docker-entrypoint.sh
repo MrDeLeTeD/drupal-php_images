2385c5420d75c6404f51551af1c39fc6592e2b34
Damien Loté <damien.lote@gmail.com>
2016-07-08 18:25:32 +0200

    remove enrtypoint

--- 7.0/apache_main/core/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Apache gets grumpy about PID files pre-existing
-#rm -f /var/run/apache2/apache2.pid
-
-#chown -R www-data:www-data /var/www/
-# Start Apache in foreground
-#/usr/sbin/apache2 -DFOREGROUND
